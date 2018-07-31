720aa41f7cd14922750b2d917fd8233b4529bcf0
Joe <joe.cheng2001@gmail.com>
2018-07-31 15:41:48 +0100

    Added checking for invalid keys for affine decrypt

--- affine.hs
+++ affine.hs
@@ -8,6 +8,9 @@ encipherAffine pt key
     | gcd (fst key) 26 /= 1 = "Invalid key, fst needs to be coprime with 26"
     | otherwise = alphabetValuesToStr $ map ((`mod` 26) . ((+ (snd key)) . (* (fst key)))) $ strToAlphabetValues pt
 
+
 decipherAffine :: String -> (Int, Int) -> String
-decipherAffine ct key = alphabetValuesToStr $ map ((`mod` 26) . ((* (fromJust (getMultiplicativeInverse (fst key)))) . (+ (-1)* (snd key)))) $ strToAlphabetValues ct
+decipherAffine ct key
+    | gcd (fst key) 26 /= 1 = "Invalid key, fst needs to be coprime with 26"
+    | otherwise = alphabetValuesToStr $ map ((`mod` 26) . ((* (fromJust (getMultiplicativeInverse (fst key)))) . (+ (-1)* (snd key)))) $ strToAlphabetValues ct
 
