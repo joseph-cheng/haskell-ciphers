52f305c0a03f4934bd223082c6187df021e72c81
Joe <joe.cheng2001@gmail.com>
2018-08-01 13:23:17 +0100

    Added index of coincidence

--- utils.hs
+++ utils.hs
@@ -26,6 +26,14 @@ sanitiseString :: String -> String
 sanitiseString st = map toUpper $ filter (isAlpha) st
 
 analyseFrequency :: String -> [(Char, Float)]
-analyseFrequency st = [(char, freq) | char <- (nub st),
-                                     let freq = (fromIntegral $ length $ filter (==char) st) / (fromIntegral $ length st)]
+analyseFrequency st = [(char, freq) | char <- ['A'..'Z'],
+                                     let freq = (fromIntegral $ countOccurrences char $ sanitiseString st) / (fromIntegral $ length $ sanitiseString st)]
 
+countOccurrences :: Eq a => a -> [a] -> Int
+countOccurrences x st = length $ filter (==x) st
+
+indexOfCoincidence :: String -> Float
+indexOfCoincidence st = sum [(fromIntegral (n * (n-1))) / (fromIntegral (textLength * (textLength-1)))  | char <- ['A'..'Z'],
+                                                                                                          let n = countOccurrences char $ sanitiseString st]
+                            where textLength = length $ sanitiseString st
+                                                        
